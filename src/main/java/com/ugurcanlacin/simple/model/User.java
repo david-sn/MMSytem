4085b257c44bdcc9ba87384e437e00117c70bf5d
David Shire <David Shire@DAVID>
2017-08-17 14:01:08 +0200

    first example JSF+Spring+Hibernate last version all

---
+++ src/main/java/com/ugurcanlacin/simple/model/User.java
@@ -0,0 +1,56 @@
+package com.ugurcanlacin.simple.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_")
+public class User implements java.io.Serializable {
+
+    private Integer id;
+    private String name;
+    private String surname;
+
+    public User() {
+    }
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", unique = true, nullable = false)
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "name", length = 20)
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "surname", length = 20)
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+}
